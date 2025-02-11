ffdaf7ea7a1bfe53c292f57044e7dbe5f31843e3
Niaz Rahman <76751075+niazrahman13@users.noreply.github.com>
2025-02-12 02:37:13 +0600

    1073 - Even Square - Beecrowd.cpp

---
+++ 1073 - Even Square - Beecrowd.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n';
+
+int main(){
+    
+    optimize();
+
+    int n;
+    cin>>n;
+
+    for(int i = 1 ; i <= n ; i++){
+        if( i % 2 == 0){
+            cout<<i<<"^"<<2<<" "<<"= "<<i*i<<endl;
+        }
+    }
+
+    return 0;
+}
