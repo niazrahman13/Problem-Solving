39d18bdca84a5329f4be5697a73932f9959d1a36
Niaz Rahman <76751075+niazrahman13@users.noreply.github.com>
2025-06-06 07:18:46 +0600

    Create Chef in his Office - CodeChef.cpp

---
+++ Chef in his Office - CodeChef.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+
+int main() {
+
+    optimize();
+
+    int t;
+    cin>>t;
+
+    for(int i = 0 ; i < t ; i++){
+        
+        int x,y;
+        cin>>x>>y;
+
+        x = x * 4 + y;
+        cout<<x<<endl;
+
+    }
+
+
+
+    return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+
+int main() {
+
+    optimize();
+
+    int t;
+    cin>>t;
+
+    for(int i = 0 ; i < t ; i++){
+        
+        int x,y;
+        cin>>x>>y;
+
+        x = x * 4 + y;
+        cout<<x<<endl;
+
+    }
+
+
+
+    return 0;
+}
