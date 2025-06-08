3dcdbe80956e2c29e08f83171970e8446641e022
Niaz Rahman <76751075+niazrahman13@users.noreply.github.com>
2025-06-09 03:46:22 +0600

    Create Count the Notebooks - CodeChef.cpp

---
+++ Count the Notebooks - CodeChef.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+
+int main() {
+
+    optimize();
+
+    int t;
+    cin>>t;
+
+    for(int i = 0 ; i < t ; i++){
+
+        int x,total;
+        cin>>x;
+
+        total = x * 1000;
+
+        cout<<total/100<<endl;
+
+    }
+    
+
+    return 0;
+}#include <bits/stdc++.h>
+using namespace std;
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+
+int main() {
+
+    optimize();
+
+    int t;
+    cin>>t;
+
+    for(int i = 0 ; i < t ; i++){
+
+        int x,total;
+        cin>>x;
+
+        total = x * 1000;
+
+        cout<<total/100<<endl;
+
+    }
+    
+
+    return 0;
+}
