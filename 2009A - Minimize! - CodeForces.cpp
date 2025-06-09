0e0183dbc5cd76d69a46e3d6ab2777071d68d5b2
Niaz Rahman <76751075+niazrahman13@users.noreply.github.com>
2025-06-09 13:48:48 +0600

    Create 2009A - Minimize! - CodeForces.cpp

---
+++ 2009A - Minimize! - CodeForces.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+
+int main() {
+
+    optimize();
+    
+    int t;
+    cin>>t;
+
+    for(int i = 0 ; i < t ; i++){
+        int x,y;
+        cin>>x>>y;
+
+        int c = abs(-x+y);
+        cout<<c<<endl;
+    }
+    
+
+    return 0;
+}
