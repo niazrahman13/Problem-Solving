9d1b526b7fd4fec43746c7b52fcd214a480d8861
Niaz Rahman <76751075+niazrahman13@users.noreply.github.com>
2025-07-01 02:44:02 +0600

    1807A - Plus or Minus

---
+++ 1807A - Plus or Minus - Codeforces.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+
+int main() {
+    optimize();
+
+    int t;
+    cin>>t;
+
+    for(auto i = 0 ; i < t; i++){
+        
+        int a,b,c;
+        cin>>a>>b>>c;
+
+        if(a + b == c){
+            cout<<'+'<<endl;
+        }else if(a - b == c){
+            cout<<'-'<<endl;
+        }
+
+
+    }
+
+    return 0;
+}
